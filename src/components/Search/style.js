12430a3887076341a75fdcbf38baaca8eb8841ad
Romany <romany.sefen@coveainsurance.co.uk>
2020-01-28 17:39:07 +0000

    moved mockdata to the public folder

--- src/components/Search/style.js
+++ src/components/Search/style.js
@@ -11,7 +11,7 @@ align-items:center;
 };
 `
 export const StyledLi = styled.div` 
-display: ${ props => props.display? props.display : 'block'}; 
+display: ${ props => props.display? props.display : 'none'}; 
 background-color: ${props => props.bg ? props.bg : props.theme.bg};
 color:${props => props.color ? props.color : props.theme.color};
 
