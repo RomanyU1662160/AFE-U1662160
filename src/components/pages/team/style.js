c0254134eb8a3071506db5cdabc817c21a647230
Romany Sefen <u1662160@unimail.hud.ac.uk>
2020-02-04 23:51:47 +0000

    fixed the chart responsive dispaly on mobile

--- src/components/pages/team/style.js
+++ src/components/pages/team/style.js
@@ -2,30 +2,31 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
-  flex-flow: row nowrap;
-  margin: 1%;
+  flex-flow: row wrap;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
 `;
 
 export const StyledRightSideMedia = styled.div`
   flex: 1;
   border: solid 1px darkgray;
+  width: 100%;
+  max-width: 100%;
 
   @media (max-width: 781px) {
     flex: 1 1 100%;
-    order: 0;
-    background-color: red;
+    color: "red";
   }
 `;
 
 export const StyledLeftSideMedia = styled.div`
   flex: 1;
   border: solid 1px darkgray;
-
+  width: 100%;
+  max-width: 100%;
   @media (max-width: 781px) {
     flex: 1 1 100%;
     order: -1;
-    background-color: red;
+    color: "yellow";
   }
 `;
