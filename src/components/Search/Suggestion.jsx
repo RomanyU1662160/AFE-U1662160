fddfbbc51f8c751a567c54fb058297d03605a84e
Romany Sefen <u1662160@unimail.hud.ac.uk>
2020-02-08 23:14:06 +0000

    Added fetch stastics data

--- src/components/Search/Suggestion.jsx
+++ src/components/Search/Suggestion.jsx
@@ -1,41 +1,39 @@
-import React, { Fragment, useContext, useState } from 'react';
-import { ThemeContext } from '../../contexts/ThemeContext';
-import { StyledLi } from './style';
-import { useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import { SearchContext } from '../../contexts/SearchContext';
-import TabGroup from '../baseComponents/tabs/TabGroup';
+import React, { Fragment, useContext, useState } from "react";
+import { ThemeContext } from "../../contexts/ThemeContext";
+import { StyledLi } from "./style";
+import { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { SearchContext } from "../../contexts/SearchContext";
 
-const Suggestion = (props) => {
-	const { suggestion, display } = props;
-	const [ selectedTeam, setSelectedteam ] = useState(null);
-	const { getTeamDetails } = useContext(SearchContext);
-	const { theme } = useContext(ThemeContext);
+const Suggestion = props => {
+  const { suggestion, display } = props;
+  const [selectedTeam, setSelectedTeam] = useState(null);
+  const { getTeamDetails } = useContext(SearchContext);
+  const { theme } = useContext(ThemeContext);
 
-	useEffect(() => {
-		console.log('display', display);
-	});
+  useEffect(() => {
+    console.log("display", display);
+  });
 
-	const handleSelect = (id) => {
-		const selected = getTeamDetails(id);
-		console.log('selected', selected);
-		return setSelectedteam(selected);
-	};
+  const handleSelect = id => {
+    const selected = getTeamDetails(id);
+    return setSelectedTeam(selected);
+  };
 
-	return (
-		<Fragment>
-			<NavLink to={'/team/' + suggestion.team.id}>
-				<StyledLi
-					className="alert border mt-2 "
-					theme={theme}
-					display={display}
-					onClick={() => handleSelect(suggestion.team.id)}
-				>
-					{suggestion ? suggestion.team.name : ''}
-				</StyledLi>
-			</NavLink>
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <NavLink to={"/team/" + suggestion.team.id}>
+        <StyledLi
+          className="alert border mt-2 "
+          theme={theme}
+          display={display}
+          onClick={() => handleSelect(suggestion.team.id)}
+        >
+          {suggestion ? suggestion.team.name : ""}
+        </StyledLi>
+      </NavLink>
+    </Fragment>
+  );
 };
 
 export default Suggestion;
