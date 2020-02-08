213293516b735c442926b272fc213667b3b88f30
Romany Sefen <u1662160@unimail.hud.ac.uk>
2020-02-08 23:16:01 +0000

    Devloped team stastis Function

---
+++ src/helpers/fetchTeamStatics.js
@@ -0,0 +1,20 @@
+export const fetchStatistics = async () => {
+  // const url = ` https://api-football-beta.p.rapidapi.com/statistics?season=2019&team=${id}&league=39`;
+
+  const response = await fetch("../../MockStatics.json");
+
+  const data = await response.json();
+  console.log(data);
+
+  return data;
+};
+
+export const fetchTeamStatistics = async id => {
+  const statistics = await fetchStatistics();
+
+  const teamStatistics = statistics.filter(data => {
+    return data.response.team.id === id;
+  });
+  // console.log("teamStatistics in helper function ::", teamStatistics);
+  return teamStatistics;
+};
