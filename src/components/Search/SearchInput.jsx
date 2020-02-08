078242ba377f09c8df5763fcc8920e80d0a2fa5e
Romany Sefen <u1662160@unimail.hud.ac.uk>
2020-02-08 23:13:19 +0000

    Refactor Suggestion component

--- src/components/Search/SearchInput.jsx
+++ src/components/Search/SearchInput.jsx
@@ -1,40 +1,37 @@
-import React, { Fragment, useEffect, useState, useContext } from 'react';
-import SuggestionsList from './SuggestionsList';
-import {Input,FormGroup} from 'reactstrap'; 
+import React, { useState, useContext } from "react";
+import SuggestionsList from "./SuggestionsList";
+import { Input, FormGroup } from "reactstrap";
 
-import { SearchContext } from '../../contexts/SearchContext';
+import { SearchContext } from "../../contexts/SearchContext";
 
+const SearchInput = props => {
+  const { data, filterResults, setData, isLoading } = useContext(SearchContext);
+  const [MatchedData, setMatchedData] = useState(data);
 
-const SearchInput = (props) => {
-	const { data, filterResults, setData, isLoading } = useContext(SearchContext);
-	const [ MatchedData, setMatchedData ] = useState(data);
+  const handleFilterSearch = input => {
+    const results = filterResults(input);
+    return setMatchedData(results);
+  };
 
-	// console.log(isLoading)
+  return (
+    <>
+      <div className="container">
+        <h3 className="text-center"> Search Form </h3>
+        <FormGroup>
+          <Input
+            type="text"
+            onChange={e => handleFilterSearch(e.target.value)}
+            placeholder="Search Teams.."
+            autoFocus
+          />
 
-	const handleFilterSearch = (input) => {
-		const results = filterResults(input);
-		// console.log("results", results );
-		return setMatchedData(results);
-	};
-
-	return (
-		<>
-			<div className="container">
-				<h3 className="text-center"> Search Form </h3>
-				<FormGroup>
-					<Input
-						type="text"
-						onChange={(e) => handleFilterSearch(e.target.value)}
-						placeholder="Search Teams.."
-						autoFocus
-					/>
-
-					<SuggestionsList suggestions={MatchedData} isLoading={isLoading}> </SuggestionsList>
-					</FormGroup>
-				</div>
-				
-		</>
-	);
+          <SuggestionsList suggestions={MatchedData} isLoading={isLoading}>
+            {" "}
+          </SuggestionsList>
+        </FormGroup>
+      </div>
+    </>
+  );
 };
 
 export default SearchInput;
