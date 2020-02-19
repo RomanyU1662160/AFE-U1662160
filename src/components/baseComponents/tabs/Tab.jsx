df14e843bb7056f27d717ac273feec9998d24613
Romany <romany.sefen@coveainsurance.co.uk>
2020-02-19 16:01:33 +0000

    fixed the uppercase first letter in team page

--- src/components/baseComponents/tabs/Tab.jsx
+++ src/components/baseComponents/tabs/Tab.jsx
@@ -1,45 +1,51 @@
-import React, { Fragment, useContext, useEffect } from 'react';
+import React, { Fragment, useContext, useEffect } from "react";
 
-import { InputItem, InputLabel, TabContent } from './style';
-import { ThemeContext } from '../../../contexts/ThemeContext';
+import { InputItem, InputLabel, TabContent } from "./style";
+import { ThemeContext } from "../../../contexts/ThemeContext";
 
 export default function Tab(props) {
-	const { type, name, id, label, content } = props;
-	const { theme } = useContext(ThemeContext);
+  const { type, name, id, label, content } = props;
+  const { theme } = useContext(ThemeContext);
 
-	return (
-		<Fragment>
-			<InputItem type={type} name={name} id={id} label={label} />
-			<InputLabel theme={theme} htmlFor={id} onClick={() => showContent(id + 'content')}>
-				{label}
-			</InputLabel>
+  return (
+    <Fragment>
+      <InputItem type={type} name={name} id={id} label={label} />
+      <InputLabel
+        theme={theme}
+        htmlFor={id}
+        onClick={() => showContent(id + "content")}
+      >
+        {label}
+      </InputLabel>
 
-			<TabContent id={id + 'content'}>
-				{Object.entries(content).map(([ key, value ]) => {
-					return (
-						<div className="row border-bottom" key={value}>
-							<div className="col-md-6"> {key.toUpperCase()} : </div>
-							<div className="col-md-6"> {value}</div>
-						</div>
-					);
-				})}
-			</TabContent>
-		</Fragment>
-	);
+      <TabContent id={id + "content"}>
+        {Object.entries(content).map(([key, value]) => {
+          return (
+            <div className="row border-bottom" key={value}>
+              <div className="col-md-6">
+                {key[0].toUpperCase() + key.slice(1)} :{" "}
+              </div>
+              <div className="col-md-6"> {value}</div>
+            </div>
+          );
+        })}
+      </TabContent>
+    </Fragment>
+  );
 }
 
-const showContent = (content) => {
-	const tabContent = document.querySelector(`#${content}`);
-	hideOtherContents(`#${content}`);
-	return (tabContent.style.display = 'block');
+const showContent = content => {
+  const tabContent = document.querySelector(`#${content}`);
+  hideOtherContents(`#${content}`);
+  return (tabContent.style.display = "block");
 };
 
-const hideOtherContents = (content) => {
-	const otherContents = [ ...document.querySelectorAll(TabContent) ];
-	otherContents.map((otherContent) => {
-		if (otherContent.id !== content) {
-			return (otherContent.style.display = 'none');
-		}
-		return (otherContent.style.display = 'block');
-	});
+const hideOtherContents = content => {
+  const otherContents = [...document.querySelectorAll(TabContent)];
+  otherContents.map(otherContent => {
+    if (otherContent.id !== content) {
+      return (otherContent.style.display = "none");
+    }
+    return (otherContent.style.display = "block");
+  });
 };
