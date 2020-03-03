19634d7ad99aa87095a8341358964a2f64f23d5c
Romany <romany.sefen@coveainsurance.co.uk>
2020-03-03 18:20:32 +0000

    Aded the test for MainNav Component

---
+++ src/__test__/Mainnav.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {render, cleanup} from "@testing-library/react";
+import {ThemeContext} from "../contexts/ThemeContext";
+import MainNav from "../components/baseComponents/main-nav/MainNav";
+import NavItem from "../components/baseComponents/mainnav-item/NavItem";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+const theme = {
+  color: "#ffc107",
+  bg: "#3e70a5",
+  ui: "black"
+};
+
+it("render Home link", () => {
+  const {getByText} = render(
+    <ThemeContext.Provider value={{theme}}>
+      <BrowserRouter>
+        <MainNav>
+          <NavItem title='Home'> </NavItem>
+        </MainNav>
+      </BrowserRouter>
+    </ThemeContext.Provider>
+  );
+  expect(getByText("Home")).toBeInTheDocument();
+});
