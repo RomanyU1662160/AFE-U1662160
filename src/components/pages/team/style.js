bd8060fc06b1f8d56bb13310849758319aca025f
Romany <romany.sefen@coveainsurance.co.uk>
2020-03-11 11:54:49 +0000

    Refactor the team logo to be more responsive on mobile view

--- src/components/pages/team/style.js
+++ src/components/pages/team/style.js
@@ -31,12 +31,37 @@ export const StyledLeftSideMedia = styled.div`
   }
 `;
 
-export const Logo = styled.div`
-  min-width: 100%;
-  min-height: 100%;
-  background-color: red;
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TeamTitle = styled.h4`
+  display: 3;
+  color: #808a81;
+  float: right;
+`;
+
+export const SmallTeamLogo = styled.img`
+  width: 25%;
+  padding: 5px;
+  @media (max-width: 781px) {
+    display: none;
+  }
+`;
+
+export const LogoWrapper = styled.div`
+  flex: 1;
   background: url(${props => (props.background ? props.background : "")});
   background-repeat: no-repeat;
   background-position: center;
   background-size: contain;
+  display: none;
+  @media (max-width: 781px) {
+    flex: 1 1 100%;
+    order: -1;
+    display: block;
+  }
 `;
