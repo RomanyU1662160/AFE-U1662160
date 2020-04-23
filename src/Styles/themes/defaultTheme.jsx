c5e2ed6a32d069a33baba47c26e5d7d9ff6c3e84
Romany Sefen <u1662160@unimail.hud.ac.uk>
2020-04-23 18:21:03 +0100

    Fixed the error og toggle theme

--- src/Styles/themes/defaultTheme.jsx
+++ src/Styles/themes/defaultTheme.jsx
@@ -1,6 +1,8 @@
 export const defaultTheme = {
   color: "#ffc107",
   bg: "#3e9c7f",
-  // bg: "#537369",
+  // bg: "#448e65",
+  //bg: "#5a886e",
+
   ui: "black"
 };
--- src/components/baseComponents/main-nav/style.js
+++ src/components/baseComponents/main-nav/style.js
@@ -1,15 +1,12 @@
-import styled from 'styled-components'; 
+import styled from "styled-components";
 
-export const StyledMainNav = styled.nav `
- display:flex;
- flex-flow: row wrap;
- flex:auto;
- align-items:center; 
- background-color: ${(props)=>  props.activeTheme.bg ? props.activeTheme.bg : 'lightgray'};
- color: ${(props)=>  props.activeTheme.color ? props.activeTheme.color : 'black'};
-
-
-  
- 
-`
-
+export const StyledMainNav = styled.nav`
+  display: flex;
+  flex-flow: row wrap;
+  flex: auto;
+  align-items: center;
+  background-color: ${props =>
+    props.activeTheme.bg ? props.activeTheme.bg : "lightgray"};
+  color: ${props =>
+    props.activeTheme.color ? props.activeTheme.color : "black"};
+`;
